aaa4ec9b71396490efa29a07fb6a2c134af49453
edge <wangweilong@xbmail.com.cn>
2023-07-01 22:13:52 +0800

    目录管理新增

--- edge_pdf/__init__.py
+++ edge_pdf/__init__.py
@@ -1,3 +1,3 @@
-from .annot_read import PDFProcessor, DataFrameDirectoryTree, DataFrameTransformer
+from .annot_read import PDFProcessor, DataFrameDirectoryTree, DataFrameTransformer,read_annot
 from .annot_add import PDFEditor
-__all__ = ["PDFProcessor", "DataFrameDirectoryTree", "DataFrameTransformer","PDFEditor"]
+__all__ = ["PDFProcessor", "DataFrameDirectoryTree", "DataFrameTransformer","PDFEditor","read_annot"]
